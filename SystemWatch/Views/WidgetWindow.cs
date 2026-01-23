81e390d99131160f87bd07c6c9db480822b68845
sujian <sujian@aecg.com.cn>
2026-01-23 17:46:44 +0800

    修正窗口位置更新

--- SystemWatch/Views/WidgetWindow.cs
+++ SystemWatch/Views/WidgetWindow.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.Win32;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Media;
@@ -20,6 +21,40 @@ namespace SystemWatch.Views
 
         [DllImport("user32.dll", EntryPoint = "SetParent")]
         static extern int SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr MonitorFromWindow(IntPtr hWnd, uint dwFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MONITORINFO
+        {
+            public uint cbSize;
+            public RECT rcMonitor;
+            public RECT rcWork;
+            public uint dwFlags;
+        }
+
+        private const uint MONITOR_DEFAULTTONEAREST = 0x00000002;
+
+        private const uint SWP_NOZORDER = 0x0004;
+        private const uint SWP_NOACTIVATE = 0x0010;
+        private const uint SWP_SHOWWINDOW = 0x0040;
+        private const uint SWP_NOSIZE = 0x0001;
         
         private readonly List<Widget> _widgets = new List<Widget>();
         
@@ -132,15 +167,15 @@ namespace SystemWatch.Views
             DoRender();
         }
         
-        private void OnScreenChanged(object? sender, EventArgs e)
+        private async void OnScreenChanged(object? sender, EventArgs e)
         {
+            await Task.Delay(2000);
             UpdateWindowPosition();
         }
 
         private void OnLoaded(object? sender, EventArgs e)
         {
             SystemEvents.DisplaySettingsChanged += OnScreenChanged;
-            OnScreenChanged(null, null);
         }
 
         protected override void OnClosed(EventArgs e)
@@ -178,6 +213,33 @@ namespace SystemWatch.Views
 
         private void UpdateWindowPosition()
         {
+            if (this._shellViewPtr != IntPtr.Zero)
+            {
+                var windowInteropHelper = new WindowInteropHelper(this);
+                IntPtr hWnd = windowInteropHelper.Handle;
+                if (hWnd != IntPtr.Zero)
+                {
+                    // 获取物理屏幕尺寸
+                    IntPtr monitor = MonitorFromWindow(this._shellViewPtr, MONITOR_DEFAULTTONEAREST);
+                    if (monitor != IntPtr.Zero)
+                    {
+                        MONITORINFO monitorInfo = new MONITORINFO();
+                        monitorInfo.cbSize = (uint)Marshal.SizeOf(typeof(MONITORINFO));
+                        if (GetMonitorInfo(monitor, ref monitorInfo))
+                        {
+                            int screenWidth = monitorInfo.rcMonitor.Right - monitorInfo.rcMonitor.Left;
+                            double screenRatio = ((double)screenWidth) / SystemParameters.PrimaryScreenWidth;
+                            // 计算相对父窗口的坐标
+                            int targetX = (int) (screenWidth - 250 * screenRatio);
+                            int targetY = (int) (120 * screenRatio);
+
+                            SetWindowPos(hWnd, IntPtr.Zero, targetX, targetY, 0, 0,
+                                SWP_NOZORDER | SWP_NOACTIVATE | SWP_NOSIZE);
+                            return;
+                        }
+                    }
+                }
+            }
             this.Left = SystemParameters.PrimaryScreenWidth - 250;
             this.Top = 120;
         }
@@ -187,9 +249,9 @@ namespace SystemWatch.Views
             IntPtr shellViewPtr = this.FindDescktopWindow();
             if (shellViewPtr != IntPtr.Zero && this._shellViewPtr != shellViewPtr)
             {
-                var windowInteropHelper = new WindowInteropHelper(this);
-                SetParent(windowInteropHelper.Handle, shellViewPtr);
-                this._shellViewPtr = shellViewPtr;
+               var windowInteropHelper = new WindowInteropHelper(this);
+               SetParent(windowInteropHelper.Handle, shellViewPtr);
+               this._shellViewPtr = shellViewPtr;
             }
         }
         
